81ede286561ae65463cf95929791c97fdc0eb501
spartanon <ira@agoras.app>
2023-01-08 19:03:15 -0500

    add tests for first instrument

--- contracts/instruments/TopYieldSwap.sol
+++ contracts/instruments/TopYieldSwap.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../../node_modules/@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 import "../interfaces/IIntegration.sol";
 
 contract TopYieldSwap is Ownable {
@@ -53,18 +54,18 @@ contract TopYieldSwap is Ownable {
     }
 
     function approve(address spender, uint256 _amount) public gtZero(_amount) {
-        IERC20 tokenContract = IERC20(yieldPools[activePool].addr);
+        IERC20 tokenContract = IERC20(tokenAddr);
         tokenContract.approve(spender, _amount);
     }
 
     function deposit(uint256 _amount) payable public gtZero(_amount) {
-        IERC20 tokenContract = IERC20(yieldPools[activePool].addr);
+        IERC20 tokenContract = IERC20(tokenAddr);
         tokenContract.transferFrom(msg.sender, address(this), _amount);
     }
 
-    function withdrawl(uint256 _amount) public onlyOwner gtZero(_amount) {
-        IERC20 tokenContract = IERC20(yieldPools[activePool].addr);
-        tokenContract.transferFrom(address(this), msg.sender, _amount);
+    function withdrawal(address _to, uint256 _amount) public gtZero(_amount) {
+        IERC20 tokenContract = IERC20(tokenAddr);
+        tokenContract.transferFrom(address(this), _to, _amount);
     }
 
     function getTokenBalance() public view returns (uint256 tokenBalance) {
@@ -93,7 +94,7 @@ contract TopYieldSwap is Ownable {
         return true;
     }
 
-    function selectPool(uint16[] calldata _poolIds, uint16 _length) internal view returns (YieldPool memory yieldPool) {
+    function selectPool(uint16[] calldata _poolIds, uint16 _length) public view returns (YieldPool memory yieldPool) {
         uint16 _topYield = 0x0;
         uint16 _poolId = 0;
         for (uint16 i = 0; i < _length; i++) {
@@ -123,6 +124,14 @@ contract TopYieldSwap is Ownable {
         delete yieldPools[_id];
         delete integrations[_id];
         emit YieldPoolRemoved(_id);
+    }
+
+    function setYieldPoolYield(uint16 _id, uint16 _yield) public onlyOwner {
+        yieldPools[_id].currentYield = _yield; 
+    }
+
+    function getYieldPool(uint16 _id) public view onlyOwner returns(YieldPool memory yieldPool) {
+        return yieldPools[_id];
     }
 
     modifier gtZero (uint256 _amount) {
---
+++ test/TopYieldSwap.js
@@ -0,0 +1,112 @@
+const { assert } = require("chai");
+
+const TopYieldSwap = artifacts.require("TopYieldSwap");
+const ERC20 = artifacts.require("ERC20PresetMinterPauser");
+
+contract("TopYieldSwap", (accounts) => {
+    const [owner, user1, user2] = accounts;
+    const txParams = { from: owner };
+
+    it("should be able to add yield pool", async() => {
+        const contractInstance = await createContractInstance1();
+        const integration = await addYieldPool1(contractInstance);
+        assert.equal(integration.receipt.status, true);
+
+        const yieldPool = await contractInstance.getYieldPool(0x1);
+        assert.equal(yieldPool.project, "HOP");
+    });
+
+    it("should be able to remove a yield pool", async() => {
+        const contractInstance = await createContractInstance1();
+        const integration = await addYieldPool1(contractInstance);
+        assert.equal(integration.receipt.status, true);
+
+        const yieldPool = await contractInstance.getYieldPool(0x1);
+        assert.equal(yieldPool.project, "HOP");
+        const removeYieldPool = await contractInstance.removeYieldPool(0x1);
+        assert.equal(removeYieldPool.receipt.status, true);
+        const removedYieldPool = await contractInstance.getYieldPool(0x1);
+        assert.notEqual(removedYieldPool.id, 0x1);        
+    });
+
+    it("should be able to set YieldPool current yield", async() => {
+        const contractInstance = await createContractInstance1();
+        const integration = await addYieldPool1(contractInstance);
+        assert.equal(integration.receipt.status, true);
+
+        const setYield1 = await contractInstance.setYieldPoolYield(0x1, 0xFF);
+        assert.equal(setYield1.receipt.status, true);
+        const yieldPool1 = await contractInstance.getYieldPool(0x1);
+        assert.equal(yieldPool1.currentYield, 0xFF);
+
+        const setYield2 = await contractInstance.setYieldPoolYield(0x1, 0xCC);
+        assert.equal(setYield2.receipt.status, true);
+        const yieldPool2 = await contractInstance.getYieldPool(0x1);
+        assert.equal(yieldPool2.currentYield, 0xCC);
+    });
+
+    it("should be able to deposit ERC20 tokens to TopYieldSwap", async() => {
+        const contractInstance = await createContractInstance1();
+
+        // ERC20PresetMinterPauser models the constraints of fiat-backed stablecoins
+        const ERC20Instance = await createERC20Instance();
+
+        const mint = await ERC20Instance.mint(user1, 0x4FFFF);
+        assert.equal(mint.receipt.status, true);
+        const transfer = await ERC20Instance.transfer(contractInstance.address, 0xFFFF, {from: user1});
+        assert.equal(transfer.receipt.status, true);
+        const despositBalance = await ERC20Instance.balanceOf(contractInstance.address);
+        assert.equal(despositBalance, 0xFFFF);
+    });
+
+    it("should be able to withdrawal ERC20 token from TopYieldSwap", async() => {
+        const ERC20Instance = await createERC20Instance();
+        const contractInstance = await createContractInstance2(ERC20Instance);
+        // ERC20PresetMinterPauser models the constraints of fiat-backed stablecoins
+
+        const mint = await ERC20Instance.mint(user1, 0x4FFFF);
+        const transfer = await ERC20Instance.transfer(contractInstance.address, 0xFFFF, {from: user1});
+        const approve = await contractInstance.approve(contractInstance.address, 0xFFFF, {from: user1});
+        const withdrawal = await contractInstance.withdrawal(user1, 0xFFFF, {from: owner});
+        assert.equal(withdrawal.receipt.status, true);
+
+        const balance = await ERC20Instance.balanceOf(user1);
+        assert.equal(balance, 0x4FFFF);
+    });
+
+    // HELPERS //
+
+    async function createContractInstance1() {
+        return await TopYieldSwap.new(
+            0x1,    // chain id
+            "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8", // token address,
+            txParams 
+        );
+    }
+
+    async function createContractInstance2(erc20) {
+        return await TopYieldSwap.new(
+            0x1,    // chain id
+            erc20.address, // token address,
+            txParams 
+        );
+    }
+
+    async function addYieldPool1(contractInstance) {
+        return await contractInstance.addYieldPool(
+            0x1,    // id 
+            "HOP",  // project
+            "USDC", // token
+            "0x10541b07d8Ad2647Dc6cD67abd4c03575dade261"
+        );
+    }
+
+    async function createERC20Instance() {
+        return await ERC20.new("USDI", "USDI");
+    }
+
+    async function mintERC20ToAccount(erc20, address) {
+        return await erc20.mint(0x4FFFF, address);
+    }
+
+});
