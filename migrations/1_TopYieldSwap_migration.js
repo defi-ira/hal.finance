f7cbd42bd6e6c1442bbcf87d82e8ef60e8fb5b7e
spartanon <ira@agoras.app>
2023-01-08 19:03:15 -0500

    started local deployment readiness

--- migrations/1_TopYieldSwap_migration.js
+++ migrations/1_TopYieldSwap_migration.js
@@ -1,6 +1,5 @@
 var topYieldSwap = artifacts.require("../contracts/instruments/TopYieldSwap.sol");
 
 module.exports = function(deployer) {
-    const usdc_testnet = 0xE9eAf0FD93B77AEE1c012E91B4a7653553949b7B;
-    deployer.deploy(topYieldSwap, 0x1, usdc_testnet);
+    deployer.deploy(topYieldSwap, 0x1, "0xE9eAf0FD93B77AEE1c012E91B4a7653553949b7B");
 }
