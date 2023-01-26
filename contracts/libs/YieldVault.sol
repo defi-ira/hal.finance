a530301913ff9a6ba8d0729ed2698b697e7662aa
spartanon <ira@agoras.app>
2023-01-25 22:39:06 -0500

    refactor YieldVault

--- contracts/libs/YieldVault.sol
+++ contracts/libs/YieldVault.sol
@@ -7,35 +7,34 @@ import "./IVaultBallot.sol";
 
 contract YieldVault is Ownable, OpenEndVault, Cooldown {
 
-    mapping(uint256 => uint256) _ballotBalance;
+    mapping(uint256 => address) _ballotAddress;
     mapping(address => bool) _minted;
     address private _vaultBallot;
+
+    event MintYieldVaultBallot(address addr_, uint256 value_);
+    event UpdateYieldVaultBallot(uint256 token_, uint256 value_);
 
     constructor(address tokenAddr_, address vaultBallot_) OpenEndVault(tokenAddr_) {
         _vaultBallot = vaultBallot_;
     }
 
     function ballotBalance(uint256 token_) public view returns (uint256 balance_) {
-        return _ballotBalance[token_];
+        return getBalance(_ballotAddress[token_]);
     }
 
-    function ballotBalance(uint256[] calldata token_, uint256 length_) public view returns (uint256 balance_) {
+    function ballotListBalance(uint256[] calldata token_, uint256 length_) public view returns (uint256 balance_) {
         uint256 total;
         for (uint256 i = 0; i < length_; i++) {
-            total += _ballotBalance[token_[i]];
+            total += getBalance(_ballotAddress[token_[i]]);
         }
         return total;
     }
 
-    function mintBallot() public depositor(msg.sender) returns (uint256 token_) {
-        _minted[msg.sender];
+    function mintBallot() public depositor(msg.sender) returns (uint256 tokenId) {
+        _minted[msg.sender] = true;
         uint256 token = IVaultBallot(_vaultBallot).mint(msg.sender);
-        updateBalance(token_);
+        emit MintYieldVaultBallot(msg.sender, getBalance(msg.sender));
         return token;
-    }
-
-    function updateBalance(uint256 token_) public {
-        _ballotBalance[token_] = this.balanceOf(msg.sender);
     }
 
     function minted(address addr_) public view returns (bool minted_) {
